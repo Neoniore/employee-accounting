67bfd8fdf9fcf7dde401112e9eaaad5398869715
TabulaRasa <lforjust@gmail.com>
2024-01-06 11:24:14 +0300

    Сделал аннотации @ResponseStatus у исключений

--- src/main/java/pro/sky/my/employeeaccounting/Exception/EmployeeNotFoundException.java
+++ src/main/java/pro/sky/my/employeeaccounting/Exception/EmployeeNotFoundException.java
@@ -1,5 +1,9 @@
 package pro.sky.my.employeeaccounting.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException {
     public EmployeeNotFoundException(String s) {
         super(s);
--- src/main/java/pro/sky/my/employeeaccounting/Exception/EmployeeStorageIsFullException.java
+++ src/main/java/pro/sky/my/employeeaccounting/Exception/EmployeeStorageIsFullException.java
@@ -1,5 +1,9 @@
 package pro.sky.my.employeeaccounting.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class EmployeeStorageIsFullException extends RuntimeException {
     public EmployeeStorageIsFullException(String s) {
         super(s);
